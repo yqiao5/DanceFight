570c7ca7889b841eadb9057443d30191f7e50324
Yu Qiao <43658645+yqiao5@users.noreply.github.com>
2019-04-17 00:10:09 -0700

    GameManager

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -18,6 +18,6 @@ public class GameManager : MonoBehaviour
 
     void Restart()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        SceneManager.LoadScene("Menu");
     }
 }
